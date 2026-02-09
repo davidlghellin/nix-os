c20668e5c8a52f8b9dabbe4b77e795a21e9324fb
David López <hola@devel0pez.com>
2026-02-09 19:01:39 +0100

    fix

--- configuration.nix
+++ configuration.nix
@@ -99,6 +99,24 @@
   programs.niri.enable = true;
 
   ##########################################################################
+  ## Polkit (Autenticación gráfica)
+  ##########################################################################
+  security.polkit.enable = true;
+  systemd.user.services.polkit-gnome-authentication-agent-1 = {
+    description = "polkit-gnome-authentication-agent-1";
+    wantedBy = [ "graphical-session.target" ];
+    wants = [ "graphical-session.target" ];
+    after = [ "graphical-session.target" ];
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+      Restart = "on-failure";
+      RestartSec = 1;
+      TimeoutStopSec = 10;
+    };
+  };
+
+  ##########################################################################
   ## XDG Portals (Wayland friendly)
   ##########################################################################
   xdg.portal = {
@@ -227,6 +245,13 @@
     brightnessctl
     playerctl
     xdg-desktop-portal-hyprland
+
+    ## Screenshots
+    grim
+    slurp
+
+    ## Polkit
+    polkit_gnome
 
     ## Clipboard
     wl-clipboard
