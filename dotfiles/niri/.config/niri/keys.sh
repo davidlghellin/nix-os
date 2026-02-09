1471bef572797a81b77b22d1cef0967cd4aadbc5
David López <hola@devel0pez.com>
2026-02-09 22:07:09 +0100

    unificar niri hyprland

--- dotfiles/niri/.config/niri/keys.sh
+++ dotfiles/niri/.config/niri/keys.sh
@@ -3,4 +3,4 @@
 
 grep -E "^\s*(Mod|Super|Alt|Shift|Ctrl|Print|XF86)"\
  $HOME/.config/niri/config.kdl |\
- fuzzel --dmenu --prompt " Buscar :>   " --match-mode=exact --no-sort --no-icons -l 30 -w 110 # for version >= 1.11.0-1
+ rofi -dmenu -i -p "Buscar tecla"
